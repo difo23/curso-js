2d1ed37485c59c4020a0c304dee0a897bac4ed83
difo23 <diffozanzan@gmail.com>
2021-02-07 13:51:43 -0400

    Create index.html and index.js

---
+++ src/index.js
@@ -0,0 +1,25 @@
+
+// LIzandro Ramirez 22 5B
+
+const saludar = (nombre) => {
+
+    console.log('Creando etiqueta h1, en el HTML!');
+
+    // Comentario de una sola linea. 
+    // El codigo comentado no lo lee el interprete de JS. 
+
+    /* 
+        Comentario de varias lineas.
+        Uso basico del DOCUMENTO OBJECT MODEL (DOM)
+        permite a JS modificar el HTML. 
+    */
+
+    const h1 = document.createElement('h1');
+    h1.innerText = `Hola, ${nombre}`;
+
+    document.body.append(h1);
+}
+
+const user = "Juancho";
+
+saludar(user)
