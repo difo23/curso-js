6b7c23c25084a2238b6db81a7d1422179e872720
difo23 <diffozanzan@gmail.com>
2021-02-07 21:36:36 -0400

    Clase #1 Uso del entorno de trabajo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 
-// LIzandro Ramirez 22 5B
+/* 
+    Clase #1: Uso del entorno de trabajo.
+        * Carpeta src/ un nombre con estandar.
+        * El nombre de los archivos index otro nombre estandar.
+        * Uso de emmet para agilizar la escritura del HTML.
+        * El Document Object Model - DOM para modificar el HTML usando el JS.
+            - Tag <script src= 'ruta o path al archivo js'/> centro del enlace HTML-JS
+        * El terminal (Command Line Interface - CLI)
+        * Comentarios de una linea y de mas de una linea.
+        * Uso del objeto document parte del DOM
+        * Crear elemento HTML con la funcion document.createElement()
+        * Modificar contenido de elemento HTML  con la propiedad o variabele element.innerText
+        * Agregar un elemento al body de mi pagina web usando document.body.append(element)
+        * Abrir index.html en el buscador 
+           - Mostrar el documento HTML por elements en el buscador(Chrome) con la etiqueta h1 creada.
+           - Mostrar los resultados de console.log() por la consola.
+        * Entregar Link al repositorio curso_js en GitHub.
+        * Fin de la clase. 
+*/
+
+// Lizandro Ramirez 22 5B
 
 const saludar = (nombre) => {
 
